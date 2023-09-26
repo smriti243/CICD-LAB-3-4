8b36850871f65ef6fb7c50bae899486ba838a8f4
SmritiRai <95621716+smriti243@users.noreply.github.com>
2023-09-26 10:17:41 +0530

    Update MyCalss.java

--- src/main/java/devops/b3/cicd/lab3/MyCalss.java
+++ src/main/java/devops/b3/cicd/lab3/MyCalss.java
@@ -7,7 +7,7 @@ public class MyCalss {
 	}
 	public void testMsg()
 	{
-		System.out.println("This is Test Message");
+		System.out.println("This is Test Messagesssss");
 	}
 
 	public static void main(String[] args) {		
